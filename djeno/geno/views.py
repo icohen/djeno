14b6035d612005e25d7238ab17f7e2a9cd571368
Ian Cohen <ianrcohen@gmail.com>
2012-03-14 19:49:40 -0400

    Update data, add person list, blank canvas, css

--- djeno/geno/views.py
+++ djeno/geno/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
+from . import models
+
 def home(request):
-    return render_to_response('geno/home.html', {},
+    people = models.Person.objects.order_by('birth__date')
+    return render_to_response('geno/home.html', 
+                              {'people': people},
                               context_instance=RequestContext(request))
--- djeno/urls.py
+++ djeno/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
-
 from djeno.geno import views
 
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
