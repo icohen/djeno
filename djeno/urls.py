88ba687b2654fc6bff3da567975aa6b8a188ed8a
Ian Cohen <ianrcohen@gmail.com>
2012-12-29 13:24:54 -0500

    update reqs to latest versions

--- djeno/urls.py
+++ djeno/urls.py
@@ -12,7 +12,7 @@ v1_api = Api(api_name='v1')
 v1_api.register(api.PersonResource())
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^api/', include(v1_api.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
